d82ce176d2eba861ae4228b94f3ff3cb1fe10332
Guptaverse <sggreat88@gmail.com>
2022-05-28 15:34:52 +0530

    kadane algo :)

---
+++ s1_kadanesAlgo.cpp
@@ -0,0 +1,20 @@
+//maximum subarray sum  
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    for(auto &x:v) cin>>x;
+
+    int maxSofar = 0;
+    int currSum =v[0];
+    for(int i =1;i<n;i++){
+        currSum = max(v[i],v[i]+currSum);
+        maxSofar = max(currSum,maxSofar);
+    }
+    cout<<maxSofar<<endl;
+}
